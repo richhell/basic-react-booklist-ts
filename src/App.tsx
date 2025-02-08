e4ed30b72b06f5915f89d172ba1468f6baaed3c4
richhell <richardhellinga@gmail.com>
2025-02-07 19:50:35 -0500

    Working but ugly. Need to fix formatting and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 import Stack from 'react-bootstrap/Stack';
 import './App.css'
-import { Button } from 'react-bootstrap'
+import Alert from "react-bootstrap/Alert";
+
+
+import { Button, Form } from 'react-bootstrap'
 import { BookItem } from './types'
 import BookList from './components/BookList.tsx'
 import { listOfBooks } from './data.ts'
 
 function App() {
+  const [showAlert, setShowAlert] = useState<boolean>(false);
   const [books, setBooks] = useState<BookItem[]>(listOfBooks);
   console.log({ listOfBooks });
+  const [newBook, setNewBook] = useState<string>("");
 
 // Add a book to the list of books.
   const addBook = () => {
-    const newBook = {
+      if (!newBook) {
+        setShowAlert(true);
+      return;
+      }
+      const book: BookItem = {
       id: books.length + 1,
-      title: "New Book",
+      title: newBook,
       completed: false,
     };
-    setBooks([...books, newBook]);
+    setBooks([...books, book]);
+    setNewBook("");
   };
 
   // Toggle the read status of a book.
@@ -40,13 +50,27 @@ function App() {
   // Display the list of books with the ability to add a book, toggle read status, and delete a book.
   return (
     <Stack gap={2}>
-      <div className="title">
+      <div className="title" style ={{ width: "500px"}}>
+      {showAlert && (
+        <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+          <Alert.Heading>Error!</Alert.Heading>
+          <p>
+            Please enter a book title before adding it to the list! The field cannot be
+            empty.
+          </p>
+        </Alert>)}
+
         <h1>Book List for 2025</h1>
+        <Form.Control 
+          type="text"
+          value={newBook}
+          onChange={(e) => setNewBook(e.target.value)}
+          placeholder="Enter a book title"
+        />
         <Button className="mt-2 mb-2" onClick={addBook}>
           Add Book
           </Button>
       </div>
-      
       <div className="p-2">
         <h5>Current List of Books to Read in 2025</h5>
         <BookList 
